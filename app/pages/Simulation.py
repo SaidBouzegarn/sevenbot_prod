b0f61adefdb3de98566d61b417ce4d2d07fdaccb
SaidB <bouzegarn.said@gmail.com>
2024-11-20 11:23:52 +0100

    adapting paths to work with streamlit

--- app/pages/Simulation.py
+++ app/pages/Simulation.py
@@ -23,7 +23,7 @@ def initialize_database():
     """Ensures that the SQLite database and user_threads table exist."""
     try:
         logger.info("Initializing database")
-        conn = sqlite3.connect('Data/dbs/users_data.db')
+        conn = sqlite3.connect('app/Data/dbs/users_data.db')
         cursor = conn.cursor()
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS user_threads (
@@ -43,7 +43,7 @@ def initialize_database():
 def get_user_threads(username: str) -> List[tuple]:
     """Retrieves all thread_ids and their creation dates for a given user."""
     try:
-        conn = sqlite3.connect('Data/dbs/users_data.db')
+        conn = sqlite3.connect('app/Data/dbs/users_data.db')
         cursor = conn.cursor()
         cursor.execute("""
             SELECT thread_id, timestamp FROM user_threads
@@ -63,7 +63,7 @@ def add_new_thread(username: str, thread_id: str):
         raise ValueError("Username and thread_id must be provided")
         
     try:
-        conn = sqlite3.connect('Data/dbs/users_data.db')
+        conn = sqlite3.connect('app/Data/dbs/users_data.db')
         cursor = conn.cursor()
         
         # Check if thread_id already exists
